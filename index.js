bfe67da12c6d16bd10acb4a0f53afd1ac0666f25
farpa <arpirez.fabio@gmail.com>
2021-12-18 21:16:38 -0300

    end of introduction to functional programming

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// programando "Imperativamente "
-var numbers= [0,1,2,3,4,5,6,7,8,9];
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i] = Math.pow(numbers[i],2);
+let emails= ['fra@gmail.com','loop@outlook.com','red@gmail.com'];
+
+//filter en reduce
+let reducer = function(acc,currentEmail, index){
+    if (index % 2 === 0) {
+        acc.push(currentEmail);
+    }
+    return acc;
 }
-console.log(numbers);
-
-
-// programando "Funcionalmente "
-var _numbers= [0,1,2,3,4,5,6,7,8,9];
-let number_=_numbers.map(function(_number){
-    return Math.pow(_number,2);
-})
-console.log(number_);
+console.time("Reducer");
+emails.reduce(reducer,[]);
+console.timeEnd("Reducer");
+console.time("Filter");
+emails.filter(function(item, index){
+    return index % 2 === 0;
+});
+console.timeEnd("Filter");
